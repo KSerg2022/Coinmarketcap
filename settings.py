25ba85aa59ffc2f0a7c72f4628fc7ded72bf920a
KSerg2022 <koshevoys@gmail.com>
2023-05-03 23:00:14 +0200

    added lists for:
    1. for query by several.

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ base_dir = Path(__file__).parent
 time_stamp = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
 
 
-# base currencies
-currencies_symbols = [
+# base currencies for query one by one
+currencies_symbols_by_one = [
     'AGV', 'TICKET', 'FS', 'BEAM', 'YOM', 'LUNA', 'SOLC', 'ATOM', 'KZEN', 'LUNC', 'VENT', 'CAKE', 'MCRT', 'ESCROW',
     'AVXL', 'ONG', 'BRO', 'SAVG', 'KDA', 'ETH', 'BSCPAD', 'SLIM', 'SAMO', 'FLM', 'USDT', 'LTC', 'ICP', 'CFX',
     'POLS', 'DOT', 'XRP', 'ETC', 'ONT', 'EOS', 'MATE', 'POOLX', 'ETHW', 'SEON', 'XCH', 'FIRO', 'RVN', 'PEAK', 'BONK',
@@ -17,10 +17,29 @@ currencies_symbols = [
     'ETHPAD', 'GAIN', 'BGS', 'HINATA', 'TON', 'SOL', 'GAME', 'LPOOL', 'ARTEM', 'AE', 'OKB', 'BNB', 'SLP', 'KOM', 'AVAX',
 ]
 
-# here can add currency
-additional_currencies_symbols = []
+# here can add currency for query one by one
+additional_currencies_symbols_by_one = []
 
-# do not change
-currencies_symbols_spec = ['CFi', ]
+# do not change for query one by one
+currencies_symbols_spec_by_one = ['CFi', ]
 
-header_columns_in_xlsx_file = ['data', 'id', 'name', 'symbol', 'price']
+
+# base currencies for query by several.
+currencies_symbols_by_several = [
+    'AGV', 'TICKET', 'FS', 'BEAM', 'YOM', 'LUNA', 'SOLC', 'ATOM', 'KZEN', 'LUNC', 'VENT', 'CAKE', 'MCRT', 'ESCROW',
+    'AVXL', 'ONG', 'BRO', 'SAVG', 'KDA', 'ETH', 'BSCPAD', 'SLIM', 'SAMO', 'FLM', 'USDT', 'LTC', 'ICP', 'CFX',
+    'POLS', 'DOT', 'XRP', 'ETC', 'ONT', 'EOS', 'MATE', 'POOLX', 'ETHW', 'SEON', 'XCH', 'FIRO', 'RVN', 'PEAK', 'BONK',
+    'MIOTA', 'BTC', 'GALA', 'WIS', 'SOLR', 'BUSD', 'ADA', 'GMI', 'BSC', 'GWT', 'CTXC', 'DOGE', 'NEAR', 'GT',
+    'HT', 'WAG', 'LZ', 'FTM', 'EJS', 'GZONE', 'AION', 'GRT', 'XAVA', 'DIA', 'DASH', 'SERO', 'PPAD', 'FLOW', 'USDC',
+    'ERG', 'ALPH', 'XLM', 'BLP', 'IDIA', 'TRBL', 'GARI', 'MATIC', 'BSCS', 'PBR', 'SWAP', 'CELO', 'DAL', 'IRON', 'ZEC',
+    'ETHPAD', 'GAIN', 'BGS', 'TON', 'SOL', 'GAME', 'LPOOL', 'ARTEM', 'AE', 'OKB', 'BNB', 'SLP', 'KOM', 'AVAX',
+]
+
+# here can add currency for query by several
+additional_currencies_symbols_by_several = []
+
+# do not change for query by several
+currencies_symbols_spec_by_several = ['CFi', ]
+
+# currency wich is not on coinmarketcap.com now
+currency_is_not_on_coinmarketcap = ['HINATA', 'RAZR',]
