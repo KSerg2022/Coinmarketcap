24faa478b7d328ed6d6d4000804d256797a4a3a0
KSerg2022 <koshevoys@gmail.com>
2023-05-16 23:02:54 +0200

    refactor files 'ex_lbank.py'

--- exchanges/ex_lbank.py
+++ exchanges/ex_lbank.py
@@ -4,6 +4,7 @@ import os
 from LBank import LBankAPI
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -16,15 +17,25 @@ class ExLbank:
         self.api = LBankAPI(self.api_key, self.private_key)
 
     def get_account(self):
-        assets = self.api.user_assets()
-        # print(assets.keys())
-        # print(assets['info']['toBtc'])
-        return assets['info']['toBtc']
+        """"""
+        currencies_account = self.api.user_assets()
+        currencies = self._normalize_data(currencies_account)
+        print(currencies)
+        return currencies
+
+    @staticmethod
+    def _normalize_data(currencies_account):
+        """"""
+        currencies = []
+        for symbol, value in currencies_account['info']['toBtc'].items():
+            if float(value) != 0:
+                currencies.append({
+                    'coin': symbol,
+                    'bal': value
+                })
+        return {os.path.splitext(os.path.basename(__file__))[0][3:]: sorted(currencies, key=lambda x: x['coin'])}
 
 
 if __name__ == '__main__':
     currencies = ExLbank()
-    for symbol, value in currencies.get_account().items():
-        # print(symbol, value)
-        if float(value) != 0:
-            print(f"{symbol} = {value}")
+    currencies.get_account()
