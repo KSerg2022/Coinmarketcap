346a58dbea50c5ca2cd4604b9febfe8acba4d6d5
KSerg2022 <koshevoys@gmail.com>
2023-05-16 23:03:20 +0200

    refactor files 'ex_bybit.py'

--- exchanges/ex_bybit.py
+++ exchanges/ex_bybit.py
@@ -5,8 +5,8 @@ import requests
 
 from MexcClient.Utils.Signature import generate_signature
 
+from collections import defaultdict
 from dotenv import load_dotenv
-
 load_dotenv()
 
 from pybit.unified_trading import HTTP
@@ -20,26 +20,44 @@ class ExBybit:
         self.apiSecret = os.environ.get('BYBIT_API_SECRET_KEY')
         self.session = HTTP(testnet=False, api_key=self.apiKey, api_secret=self.apiSecret)
 
-    def get_account(self):
+    def get_account_spot(self):
         """"""
         return self.session.get_spot_asset_info()
-        # return self.session.get_wallet_balance(accountType="SPOT")
 
     def get_account_margin(self):
         """"""
         return self.session.get_wallet_balance(accountType="CONTRACT")
 
+    def get_account(self):
+        account_spot = self.get_account_spot()
+        account_margin = self.get_account_margin()
+        currencies = self._normalize_data(account_spot,
+                                          account_margin)
+        print(currencies)
+        return currencies
+
+    @staticmethod
+    def _normalize_data(account_spot, account_margin):
+        """"""
+        q = defaultdict(list)
+        for symbol in account_spot['result']['spot']['assets']:
+            q[symbol['coin']].append(float(symbol['free']) + float(symbol['frozen']))
+
+        for symbol in account_margin['result']['list'][0]['coin']:
+            if symbol['coin'] in q:
+                q[symbol['coin']] = [q[symbol['coin']][0] + float(symbol['equity'])]
+            else:
+                q[symbol['coin']].append(float(symbol['equity']))
+
+        currencies = []
+        for currency, value in q.items():
+            currencies.append({
+                'coin': currency,
+                'bal': value[0]
+            })
+        return {os.path.splitext(os.path.basename(__file__))[0][3:]: sorted(currencies, key=lambda x: x['coin'])}
+
 
 if __name__ == '__main__':
     c = ExBybit()
     currencies = c.get_account()
-    currencies = currencies['result']['spot']['assets']
-    for symbol in currencies:
-        print(f"{symbol['coin']} = {float(symbol['free']) + float(symbol['frozen'])}")
-
-    currencies = c.get_account_margin()
-    currencies = currencies['result']['list']
-    # print(len(currencies), currencies[0])
-    for symbol in currencies[0]['coin']:
-        print(f"{symbol['coin']} = {float(symbol['equity'])},"
-              f" (walletBalance={symbol['walletBalance']} + unrealisedPnl={symbol['unrealisedPnl']})")
