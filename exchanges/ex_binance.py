187f240c0f9302da2a35fe9dbb13bccc92d3578c
KSerg2022 <koshevoys@gmail.com>
2023-05-17 17:42:22 +0200

    refactor files 'ex_binance.py'

--- exchanges/ex_binance.py
+++ exchanges/ex_binance.py
@@ -1,63 +1,72 @@
 """
 https://dev.binance.vision/t/ip-whitelist-does-not-have-effect/2767
+https://algotrading101.com/learn/binance-python-api-guide/
 """
 import os
 import json
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
-from binance.spot import Spot
+# from binance.spot import Spot
+from binance.client import Client
 
 from settings import base_dir, time_stamp
 
 
-api_key = os.environ.get('BINANCE_API_KEY')
-api_secret = os.environ.get('BINANCE_API_SECRET_KEY')
+class ExBinance:
+    host = 'https://api.binance.com'
+
+    def __init__(self):
+        self.api_key = os.environ.get('BINANCE_API_KEY')
+        self.api_secret = os.environ.get('BINANCE_API_SECRET_KEY')
+
+        self.coin_m = Client(api_key=self.api_key, api_secret=self.api_secret)
+
+    def get_futures_coin_account(self):
+        futures_coin_account = self.coin_m.futures_coin_account()
+        return [x for x in futures_coin_account['assets'] if float(x['walletBalance']) != 0]
+
+    def get_spot_account(self):
+        spot_account = self.coin_m.get_account()
+        return [x for x in spot_account['balances'] if float(x['free']) != 0 or float(x['locked']) != 0]
+
+    def get_account(self):
+        spot_account = self.get_spot_account()
+        futures_coin_account = self.get_futures_coin_account()
+        currencies = self._normalize_data(spot_account,
+                                          futures_coin_account)
+
+        return currencies
+
+    @staticmethod
+    def _normalize_data(spot_account, futures_coin_account):
+        """"""
+        currencies = []
+        for symbol in spot_account + futures_coin_account:
+            if y := [x for x in currencies if x['coin'] == symbol['asset']]:
+                try:
+                    q = symbol['free'] + symbol['locked']
+                except KeyError:
+                    q = symbol['marginBalance']
+                currencies[currencies.index(y[0])] = {'coin': symbol['asset'],
+                                                      'bal': y[0]['bal'] + float(q)}
+            else:
+                try:
+                    currencies.append({
+                        'coin': symbol['asset'],
+                        'bal': float(symbol['free']) + float(symbol['locked'])
+                    })
+                except KeyError:
+                    currencies.append({
+                        'coin': symbol['asset'],
+                        'bal': float(symbol['marginBalance'])
+                    })
+        return {os.path.splitext(os.path.basename(__file__))[0][3:]: sorted(currencies, key=lambda x: x['coin'])}
 
 
-client = Spot(api_key=api_key, api_secret=api_secret, base_url='https://api.binance.com')
-
-
-# print(client.account())
-account = client.account()
-
-print(account.keys())
-print(account['permissions'])
-
-for symbol in account['balances']:
-    if (float(symbol['free']) > 0) or (float(symbol['locked']) > 0):
-        # print(f'{symbol["asset"]} - free={symbol["free"]}, locked={symbol["locked"]},'
-        #       f'TOTAL ={float(symbol["free"]) + float(symbol["locked"])}')
-
-        print(f'{symbol["asset"]} = {float(symbol["free"]) + float(symbol["locked"])}')
-
-print('~' * 50)
-result = client.account_snapshot(type="SPOT")
-for symbol in result['snapshotVos'][-1]['data']['balances']:
-    if (float(symbol['free']) > 0) or (float(symbol['locked']) > 0):
-        print(f"{symbol['asset']} = {float(symbol['free']) + float(symbol['locked'])}")
-
-print('~' * 50)
-# print(client.account_snapshot(type="MARGIN"))
-print(client.account_snapshot(type="FUTURES"))
-print(len(client.coin_info()), client.coin_info()[:3])
-print(client.user_asset())
-print(client.api_key_permissions())
-print(client.api_key_permissions())
-
-print('~' * 50)
-
-
-print('~' * 50)
-
-
-
-
-def wright_to_json(data: list[dict] | dict[dict]):
-    """Writing to file human-readable data."""
-    filename = base_dir / 'json_files' / f'binance_{time_stamp}.json'
-    with open(filename, 'w') as file_json:
-        json.dump(data, file_json, indent=4)
-
-# wright_to_json(client.account())
+if __name__ == '__main__':
+    r = ExBinance()
+    result = r.get_account()
+    print(result)
