c4416a81a0a70b11f6ef5d2120216d70cbdad922
KSerg2022 <koshevoys@gmail.com>
2023-05-22 20:40:34 +0200

    refactor 'bsc.py'
    add 'load_dotenv'
    remobe 'params' to 'init' ('self.params')
    add handling the result when there is an error

--- blockchains/bsc.py
+++ blockchains/bsc.py
@@ -18,9 +18,19 @@ class Bsc(Base):
         self.api_key = os.environ.get('BSCSCAN_API_KEY')
         self.wallet = os.environ.get('WALLET_ADDRESS')
         self.currencies = BSC_CURRENCIES
+        self.params = {'module': 'account',
+                      'action': 'tokenbalance',
+                      'contractaddress': '',
+                      'address': self.wallet,
+                      'tag': 'latest',
+                      'apikey': self.api_key,
+                      }
+
 
     def get_account(self) -> dict[dict]:
         currencies = self._get_account()
+        if 'NOTOK' in list(currencies[0].values()):
+            return {blockchain: currencies}
         return {blockchain: sorted(currencies, key=lambda x: x['coin'])}
 
 
