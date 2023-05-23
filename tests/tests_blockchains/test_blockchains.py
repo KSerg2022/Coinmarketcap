21a6fc0651d0ab2eba8261cedcb20ccbcb0e6be8
KSerg2022 <koshevoys@gmail.com>
2023-05-23 21:37:22 +0200

    create modul 'test_blockchains.py'
    where moduls - 4 files:
    - test_bsc.py
    - test_ether.py
    - test_fantom.py
    - test_polygon.py

--- tests/tests_blockchains/test_blockchains.py
+++ tests/tests_blockchains/test_blockchains.py
@@ -1,10 +1,15 @@
 """"""
 import unittest
-import json
+
 
 from pathlib import Path
 
 from blockchains.bsc import Bsc
+from blockchains.polygon import Polygon
+from blockchains.ether import Ether
+from blockchains.fantom import Fantom
+from tests.tests_blockchains.test_base import (TestBaseGetRequest,
+                                                TestBaseGetAccount)
 
 from dotenv import load_dotenv
 
@@ -13,160 +18,45 @@ load_dotenv()
 base_dir = Path(__file__).parent.parent
 
 
-class TestCmc(unittest.TestCase):
+class TestBsc(unittest.TestCase, TestBaseGetRequest, TestBaseGetAccount):
 
     def setUp(self) -> None:
-        self.bsc = Bsc()
+        self.b_chain = Bsc()
+        self.contract_address = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'
 
     def tearDown(self) -> None:
-        del self.bsc
-
-    @staticmethod
-    def get_headers(api_cmc):
-        return {'Accepts': 'application/json', 'X-CMC_PRO_API_KEY': api_cmc}
-
-    @staticmethod
-    def get_parameters(symbols):
-        return {'symbol': symbols, 'convert': 'USD'}
-
-    @staticmethod
-    def _get_params(module='account',
-                    action='tokenbalance',
-                    contractaddress='0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
-                    wallet=None,
-                    apikey=None):
-        return {'module': module,
-                'action': action,
-                'contractaddress': contractaddress,
-                'address': wallet,
-                'tag': 'latest',
-                'apikey': apikey,
-                }
-
-    def test__get_request(self):
-        params = self._get_params(wallet=self.bsc.wallet,
-                                  apikey=self.bsc.api_key)
-        result = self.bsc._get_request(self.bsc.host, params)
-
-        self.assertTrue(isinstance(result, dict))
-        self.assertEqual(result['message'], 'OK')
-        self.assertTrue(isinstance(int(result['result']), int))
-
-    def test__get_request_with_wrong_contractaddress(self):
-        params = self._get_params(contractaddress='0',
-                                  wallet=self.bsc.wallet,
-                                  apikey=self.bsc.api_key)
-        result = self.bsc._get_request(self.bsc.host, params)
-
-        self.assertEqual(result['message'], 'NOTOK')
-        self.assertEqual(result['result'], 'Invalid contractAddress format')
-
-    def test__get_request_with_wrong_api(self):
-        params = self._get_params(wallet=self.bsc.wallet,
-                                  apikey='wrong')
-        result = self.bsc._get_request(self.bsc.host, params)
-
-        self.assertEqual(result['message'], 'NOTOK')
-        self.assertEqual(result['result'], 'Invalid API Key')
-
-    def test__get_request_with_empty_api(self):
-        params = self._get_params(wallet=self.bsc.wallet,
-                                  apikey='')
-        result = self.bsc._get_request(self.bsc.host, params)
-
-        self.assertIn('OK-Missing/Invalid API Key', result['message'])
-
-    def test__get_request_with_wrong_wallet(self):
-        params = self._get_params(wallet='wrong',
-                                  apikey=self.bsc.api_key)
-        result = self.bsc._get_request(self.bsc.host, params)
-
-        self.assertEqual(result['message'], 'NOTOK')
-        self.assertEqual(result['result'], 'Error! Invalid address format')
-
-    def test__get_request_with_wrong_host(self):
-        params = self._get_params(wallet=self.bsc.wallet,
-                                  apikey=self.bsc.api_key)
-        result = self.bsc._get_request('https://api.bscscan.com/wrong', params)
-
-        self.assertEqual(result['message'], 'NOTOK')
-        self.assertEqual(result['result'], "Ошибка получения данных. Код ответа: 404")
-
-    def test__get_request_with_wrong_module(self):
-        params = self._get_params(module='wron',
-                                  wallet=self.bsc.wallet,
-                                  apikey=self.bsc.api_key)
-        result = self.bsc._get_request(self.bsc.host, params)
-
-        self.assertEqual(result['message'], 'NOTOK')
-        self.assertEqual(result['result'],
-                         "Error! Missing Or invalid Module name")
-
-    def test__get_request_with_wrong_action(self):
-        params = self._get_params(action='wron',
-                                  wallet=self.bsc.wallet,
-                                  apikey=self.bsc.api_key)
-        result = self.bsc._get_request(self.bsc.host, params)
-
-        self.assertEqual(result['message'], 'NOTOK')
-        self.assertEqual(result['result'],
-                         "Error! Missing Or invalid Action name")
-
-    # def test__get_request_status_not_200(self):
-    #     """нужно черезх мок"""
-    #     pass
+        del self.b_chain
 
 
-class testBscGetAccount(TestCmc):
+class TestPolygon(unittest.TestCase, TestBaseGetRequest, TestBaseGetAccount):
 
-    def test_get_account(self):
-        result = self.bsc.get_account()
+    def setUp(self) -> None:
+        self.b_chain = Polygon()
+        self.contract_address = '0x0000000000000000000000000000000000001010'
 
-        self.assertTrue(isinstance(result, dict))
-        self.assertTrue(isinstance(list(result.values())[0], list))
-        self.assertIn(self.bsc.COIN, list(result.values())[0][0])
-        self.assertIn(self.bsc.BAL, list(result.values())[0][0])
+    def tearDown(self) -> None:
+        del self.b_chain
 
-    def test_get_account_with_wrong_api(self):
-        self.bsc.params = self._get_params(wallet=self.bsc.wallet,
-                                            apikey='wrong')
-        result = self.bsc.get_account()
 
-        self.assertEqual(list(result.values())[0][0]['message'], 'NOTOK')
-        self.assertEqual(list(result.values())[0][0]['result'], 'Invalid API Key')
+class TestEther(unittest.TestCase, TestBaseGetRequest, TestBaseGetAccount):
 
-    def test_get_account_with_wrong_wallet(self):
-        self.bsc.params = self._get_params(wallet='wrong',
-                                            apikey=self.bsc.api_key)
-        result = self.bsc.get_account()
+    def setUp(self) -> None:
+        self.b_chain = Ether()
+        self.contract_address = '0x2170ed0880ac9a755fd29b2688956bd959f933f8'
 
-        self.assertEqual(list(result.values())[0][0]['message'], 'NOTOK')
-        self.assertEqual(list(result.values())[0][0]['result'], 'Error! Invalid address format')
+    def tearDown(self) -> None:
+        del self.b_chain
 
-    def test_get_account_with_wrong_host(self):
-        self.bsc.host = 'https://api.bscscan.com/wrong'
-        result = self.bsc.get_account()
 
-        self.assertEqual(list(result.values())[0][0]['message'], 'NOTOK')
-        self.assertEqual(list(result.values())[0][0]['result'], "Ошибка получения данных. Код ответа: 404")
+class TestFantom(unittest.TestCase, TestBaseGetRequest, TestBaseGetAccount):
 
-    def test_get_account_with_wrong_host_(self):
-        self.bsc.host = 'ttps://api.bscscan.com/api'
-        result = self.bsc.get_account()
+    def setUp(self) -> None:
+        self.b_chain = Fantom()
+        self.contract_address = '0xE705aF5f63fcaBDCDF5016aA838EAaac35D12890'
 
-        self.assertEqual(list(result.values())[0][0]['message'], 'NOTOK')
-        self.assertEqual(list(result.values())[0][0]['result'],
-                         "No connection adapters were found for 'ttps://api.bscscan.com/api'")
-
-    def test_get_account_with_wrong_currencies(self):
-        self.bsc.currencies = {
-            'BUSD': '0',
-            'CAKE': '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82_', }
-        result = self.bsc.get_account()
-
-        self.assertEqual(list(result.values())[0][0]['message'], 'NOTOK')
-        self.assertEqual(list(result.values())[0][0]['result'], 'Invalid contractAddress format')
+    def tearDown(self) -> None:
+        del self.b_chain
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=1)
